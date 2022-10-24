61039a50981913d435f0c9a51238ec25eb08064b
BrennanMcl <bdmclaughlin02@gmail.com>
2022-10-24 19:06:23 -0400

    Completed unit test

--- tests/unit/test_create_movie.py
+++ tests/unit/test_create_movie.py
@@ -1,5 +1,12 @@
 # TODO: Feature 2
-from app import app, movie_repository
+from src.repositories.movie_repository import get_movie_repository
+from src.models.movie import Movie
 
 def test_create_movie():
-    pass
+    movie_repository = get_movie_repository()
+    movie_repository.create_movie("Batman", "Robin", 3)
+
+    movie_searched = movie_repository.get_movie_by_title("Batman")
+    test_movie = Movie("Batman", "Robin", 3)
+
+    assert movie_searched.director == test_movie.director
