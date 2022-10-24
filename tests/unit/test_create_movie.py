ad6750a04e3ac348cab1aa62dfff49d826649649
BrennanMcl <bdmclaughlin02@gmail.com>
2022-10-24 18:49:59 -0400

    Minor commit with test changes

--- tests/unit/test_create_movie.py
+++ tests/unit/test_create_movie.py
@@ -1,6 +1,5 @@
 # TODO: Feature 2
-from app import create_movie
-from src.repositories.movie_repository import get_movie_repository
+from app import app, movie_repository
 
 def test_create_movie():
     pass
