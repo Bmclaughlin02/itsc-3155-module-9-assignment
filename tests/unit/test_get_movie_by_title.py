e2554893dbd0a43d3dfc678e6096a901dc4c07b2
Twoshawns <33672021+Twoshawns@users.noreply.github.com>
2022-10-25 17:21:55 -0400

    Update test_get_movie_by_title.py
    Completed e2e test

--- tests/unit/test_get_movie_by_title.py
+++ tests/unit/test_get_movie_by_title.py
@@ -1,1 +1,13 @@
 # TODO: Feature 3
+from src.repositories.movie_repository import get_movie_repository
+from src.models.movie import Movie
+
+def test_get_movie_by_title():
+    movie_repository = get_movie_repository()
+
+    movie_searched = movie_repository.get_movie_by_title("Pokemon")
+    test_movie = Movie("pokemon", "Satoshi", 5)
+
+    assert movie_searched.title == test_movie.title
+    assert movie_searched.director == test_movie.director
+    assert movie_searched.rating == test_movie.rating
