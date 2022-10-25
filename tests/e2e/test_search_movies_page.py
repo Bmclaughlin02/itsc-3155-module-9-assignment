20b18dfb635b06715f6e6d0f723fe81b3dd9be4d
Twoshawns <33672021+Twoshawns@users.noreply.github.com>
2022-10-25 17:22:24 -0400

    Update test_search_movies_page.py
    completed e2e test

--- tests/e2e/test_search_movies_page.py
+++ tests/e2e/test_search_movies_page.py
@@ -1,1 +1,19 @@
 # TODO: Feature 3
+from app import app, movie_repository
+from src.models.movie import Movie
+
+def test_get_movies_by_title_page():
+    client = app.test_client()
+
+    response = client.get("/movies/search", data={
+        "name": "Pokemon",
+        "director": "Satoshi",
+        "rating": "5 Stars",
+    })
+
+    movie = movie_repository.get_movie_by_title("Pokemon")
+    confirm_movie = Movie("Pokemon", "Satoshi", 5)
+    
+    assert movie.title == confirm_movie.title
+    assert movie.director == confirm_movie.director
+    assert movie.rating == confirm_movie.rating
